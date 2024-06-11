44f7acc640d7cdae7f4e17529fd5f77496402782
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2024-06-11 14:47:56 +0700

    feat: echo server via grpc

--- src/app/mrl-coordinator.rs
+++ src/app/mrl-coordinator.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, Coordinator");
+use tonic::{transport::Server, Request, Response, Status};
+
+use coordinator::coordinator_server::{Coordinator, CoordinatorServer};
+use coordinator::{MessageRequest, MessageResponse};
+
+pub mod coordinator {
+    tonic::include_proto!("coordinator");
+}
+
+#[derive(Debug, Default)]
+pub struct MyCoordinator {}
+
+#[tonic::async_trait]
+impl Coordinator for MyCoordinator {
+    async fn echo(
+        &self,
+        request: Request<MessageRequest>,
+    ) -> Result<Response<MessageResponse>, Status> {
+        println!("Got a request from {:?}", request.remote_addr());
+
+        let reply = MessageResponse {
+            message: format!("Hello {}!", request.into_inner().message),
+        };
+        Ok(Response::new(reply))
+    }
+}
+
+// Note: Move this out later.
+const PORT: u16 = 8030;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = format!("[::1]:{}", PORT).parse().unwrap();
+    let greeter = MyCoordinator::default();
+
+    println!("CoordinatorServer listening on {}", addr);
+
+    Server::builder()
+        .add_service(CoordinatorServer::new(greeter))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
--- src/app/mrl-ctl.rs
+++ src/app/mrl-ctl.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, mrctl");
+use std::io::stdin;
+
+use coordinator::{coordinator_client::CoordinatorClient, MessageRequest};
+
+pub mod coordinator {
+  tonic::include_proto!("coordinator");
+}
+
+const PORT: u16 = 8030;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+  let mut client = CoordinatorClient::connect(format!("http://[::1]:{}", PORT)).await?;
+  loop {
+    println!("\nInput message");
+    let mut message: String = String::new();
+    stdin().read_line(&mut message).unwrap();
+    let message = message.trim();
+    let request = tonic::Request::new(MessageRequest {
+      message: String::from(message),
+    });
+    let response = client.echo(request).await?;
+    println!("Got: '{}' from service", response.into_inner().message);
+  }
+  Ok(())
 }

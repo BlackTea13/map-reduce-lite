70016bc7673f33ecd79aca2f7489f21c4d58aa00
Robert Blum <robertblum2001@gmail.com>
2024-06-28 01:54:18 +0700

    chore: remove unused deps

--- mrl-coordinator/build.rs
+++ mrl-coordinator/build.rs
@@ -1,5 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::compile_protos("../protos/coordinator.proto")?;
     tonic_build::compile_protos("../protos/worker.proto")?;
+    println!("yoa");
     Ok(())
 }

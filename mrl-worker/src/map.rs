ee7b9c8327e8431d5443f2f67a4fafd67883907c
karank85 <kkjas85@gmail.com>
2024-07-02 00:43:57 +0700

    chore: comment testing code

--- mrl-worker/src/map.rs
+++ mrl-worker/src/map.rs
@@ -49,9 +49,10 @@ pub async fn perform_map(
     client: &Client,
     coordinator_address: &String,
 ) -> Result<(), Error> {
-    if *worker_id & 1 == 1 {
-        tokio::time::sleep(tokio::time::Duration::from_secs(10000)).await;
-    }
+    /// TODO: Remove me when straggler is done
+    // if *worker_id & 1 == 1 {
+    //     tokio::time::sleep(tokio::time::Duration::from_secs(10000)).await;
+    // }
     let bucket_in = request.bucket_in;
     let bucket_out = request.bucket_out;
     let output_key = request.output_path;
